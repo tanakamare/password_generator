92b7220e9602527867beeef333da572ffcfd0884
Tanaka Delan <maredelan@gmail.com>
2025-05-16 17:10:40 +0200

    Update password.py

--- password_Generator/password.py
+++ password_Generator/password.py
@@ -2,5 +2,17 @@
 Python password Generate
 '''
 
+import random
 
+lowwer_case = "qwertyuiopasdfghjklzxcvbnm"
+upper_case = "QWERTYUIOPASDFGHJKLZXCVBNM"
+number = "1234567890"
+symbols = "@#$%&*/\?"
+
+user_for = lowwer_case + upper_case + number + symbols
+length_password = 8
+
+password = "".join(random.sample(user_for,length_password))
+
+print(f"Your password is: {password}")
 
